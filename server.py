0341a092c37a505aad03f26ebffe49ceeeb181b3
Ryan Harris <rharris4@mail.umw.edu>
2016-11-02 23:36:48 +0000

    functional flask template code added

---
+++ server.py
@@ -0,0 +1,31 @@
+import os
+import random
+import psycopg2
+import psycopg2.extras
+import sys
+
+# need this to prevent UnicodeDecodeError
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+def connectToDB():
+  connectionString = 'dbname=helix user=helix password=helix123 host=localhost'
+  print connectionString
+  try:
+    return psycopg2.connect(connectionString)
+  except:
+    print("Can't connect to database")
+
+
+@app.route('/', methods=['GET', 'POST'])
+def mainIndex():
+    
+    return render_template('index.html')
+
+# start the server
+if __name__ == '__main__':
+    app.run(host=os.getenv('IP', '0.0.0.0'), port =int(os.getenv('PORT', 8080)), debug=True)

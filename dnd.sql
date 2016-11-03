16ab4fb5533a5a698fdf2dd43a42983b98e71202
Tyler Moore <tyler.moore.174@gmail.com>
2016-11-03 02:21:45 +0000

    Added table structure for classes and feats

--- dnd.sql
+++ dnd.sql
@@ -21,3 +21,51 @@ CREATE TABLE race (
     traits text NOT NULL DEFAULT '',
     PRIMARY KEY (ID)
 );
+
+--
+-- Table Dump for Race
+--
+
+
+
+--
+-- Table Structure for Class
+--
+
+DROP TABLE IF EXISTS class;
+CREATE TABLE class (
+    ID serial NOT NULL,
+    name varchar(50) NOT NULL default '',
+    role varchar(50) NOT NULL default '',
+    source varchar(50) NOT NULL default '',
+    keystat varchar(50) NOT NULL default '',
+    armor varchar(80) NOT NULL default '',
+    weapon varchar(150) NOT NULL default '',
+    implement varchar(50) NOT NULL default '',
+    defense varchar(50) NOT NULL default '',
+    starthp integer NOT NULL default '0',
+    levelhp integer NOT NULL default '0',
+    surge integer NOT NULL default '0',
+    skills text NOT NULL default '',
+    features text NOT NULL default '',
+    PRIMARY KEY (ID)
+);
+
+--
+-- Table Dump for Class
+--
+
+
+
+--
+-- Table Structure for Feats
+--
+
+DROP TABLE IF EXISTS feats;
+CREATE TABLE feats (
+    ID serial NOT NULL,
+    name varchar(50) NOT NULL default '',
+    prereq text NOT NULL default '',
+    benefit text NOT NULL default '',
+    PRIMARY KEY (ID)
+);
